6547c3b7b27385904cf4ca0c1b7b07e4550238f4
jhbaek <juhuiyang@naver.com>
2025-07-14 08:54:47 +0900

    수정

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/viver/', // GitHub Pages에 배포할 때 필요한 설정
+  base: '/VIVER/', // GitHub Pages에 배포할 때 필요한 설정
 })
